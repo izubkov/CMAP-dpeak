9ddc5a6350050d66744f6583f432e2623fa50903
izubkov <izybkov@gmail.com>
2018-12-19 06:22:51 +0300

    influence of speed up

--- score_tracking.R
+++ score_tracking.R
@@ -1,15 +1,13 @@
-library(plot3Drgl)
+#library(plot3Drgl)
+#f <- function(x,y)
+#  (x^2) * (y^2)
+#x <- y <- seq(0,1,len=20)
+#z <- outer(x, y, f)
+#persp3Drgl(z=z)
 
-f <- function(x,y) log(x^2 * y^2)
-x <- y <- seq(-4,4,len=20)
-z <- outer(x, y, f)
-persp3Drgl(z=z)
-
-
-f <- function(x,y)
-  (x^2) * (y^2)
-x <- y <- seq(0,1,len=20)
-z <- outer(x, y, f)
-persp3Drgl(z=z)
-
-plot(function(x) exp(-x), xlim = c(0, 1))
+t.bench <- 162
+exp_mul <- function(t) exp(-t/(3*t.bench))
+plot(function(t) exp_mul(t), xlim = c(0, t.bench), ylim = c(0.7, 1.0),
+     xlab = "t", ylab = "exponent multiplier")
+abline(h = exp_mul(0.9*t.bench), col = "red")
+abline(v = log(1/0.99)*3*t.bench, col = "green")
