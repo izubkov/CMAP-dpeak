d6e888d6ea1fb722eaaa4bb8ef10bd983c060aea
izubkov <izybkov@gmail.com>
2018-12-21 19:58:49 +0300

    ground truth

--- EDA.R
+++ EDA.R
@@ -3,20 +3,23 @@ library(purrr)
 
 DPK.files <- list.files("competitor_pack_v2/input/DPK.CP001_A549_24H_X1_B42/",
                         full.names = T)
-DPK.txt <-
+DPK.all.txt <-
   map_dfr(DPK.files, read_tsv)
 
 LIT.files <- list.files("competitor_pack_v2/input/LITMUS.KD017_A549_96H_X1_B42/",
                         full.names = T)
-LIT.txt <-
+LIT.all.txt <-
   map_dfr(DPK.files, read_tsv)
 
-all(DPK.txt %>%
+all(DPK.all.txt %>%
       select(barcode_id) %>%
       distinct() ==
-      LIT.txt %>%
+      LIT.all.txt %>%
       select(barcode_id) %>%
       distinct())
 
 barcode_to_gene_map.txt <-
   read_tsv("competitor_pack_v2/input/barcode_to_gene_map.txt")
+
+DPK.gct <-
+  read_tsv("competitor_pack_v2/ground-truth/DPK.CP001_A549_24H_X1_B42_DECONV_UNI.gct", skip = 2)
