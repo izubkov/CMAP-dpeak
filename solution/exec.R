ad366bf12b4aee791e4b1c6ab0b3c1885bcf55ff
izubkov <izybkov@gmail.com>
2018-12-31 13:49:14 +0300

    calc. kmeans

--- solution/exec.R
+++ solution/exec.R
@@ -17,6 +17,7 @@ stopifnot(length(args) == 2)
 print(args[1])
 print(args[2])
 
+# TODO: medians of k-mean separated clusters
 plate_wide_processing <- function(d.all) {
   # TODO: matrix?
   res <- tibble(gene_hi = character(), center_hi = double(),
@@ -36,20 +37,47 @@ plate_wide_processing <- function(d.all) {
 
     li <- list(x = xs, centers = 2, algorithm = "MacQueen")
     k <- run_kmeans(kmeans, li)
+    #k <- tryCatch({run_kmeans(kmeans, li)},
+    #              warning = function(w) {
+    #                cat(barcode, "does not converges in 10 iterations")
+    #                data.frame(size = c(0, 0), centers = c(0, 0))
+    #              })
+    if(k$size[1] > k$size[2]) {
+      hi <- k$centers[1]
+      lo <- k$centers[2]
+    } else {
+      hi <- k$centers[2]
+      lo <- k$centers[1]
+    }
 
-    # ...
+    genes <- barcode_to_gene_map.txt %>% filter(barcode_id == barcode)
+    if(nrow(genes) == 2) { # barcodes 11 and 499
+      gene.hi <-
+        genes[genes$high_prop == 1, ] %>%
+        .[["gene_id"]] %>%
+        as.character()
+      gene.lo <-
+        genes[genes$high_prop == 0, ] %>%
+        .[["gene_id"]] %>%
+        as.character()
+
+      res %<>% add_row(gene_hi = gene.hi, center_hi = hi,
+                       gene_lo = gene.lo, center_lo = lo)
+    }
   }
+  res
 }
 
 run_kmeans <- function(f, li) {
   k <- do.call(what = f, args = li)
 }
 
-k <- kmeans(xs, 2, algorithm = "MacQueen")
-k$centers # [1] [2]
-k$size # [1] [2]
-
-barcode_to_gene_map.txt %>% filter(barcode_id == 64)
+tic("DPK")
+dpk <- plate_wide_processing(DPK.all.txt)
+toc()
+tic("LIT")
+lit <- plate_wide_processing(LIT.all.txt)
+toc()
 
 temp_gct <-
   cmapR::parse.gctx("ground-truth/DPK.CP001_A549_24H_X1_B42_DECONV_UNI.gct")
@@ -61,3 +89,9 @@ cmapR::parse.gctx(str_c("ground-truth/", args[1], "_DECONV_UNI.gct")) %>%
 
 #BiocManager::install("rhdf5") # also install/update callr, mgcv, codetools
 #devtools::install_local("competitor_pack_v2/scorer/cmapR")
+
+# clear warnings
+assign("last.warning", NULL, envir = baseenv())
+
+# stop on warnings
+options(warn = 2)
