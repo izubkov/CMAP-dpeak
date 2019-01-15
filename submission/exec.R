78c643f6b1e0ac3f873fe090791be9df05ecd0b3
izubkov <izybkov@gmail.com>
2019-01-15 21:08:58 +0300

    swap statistically wrong high/low assigned genes

--- submission/exec.R
+++ submission/exec.R
@@ -57,7 +57,6 @@ barcode_to_gene_map.txt <-
 
 # submission --------------------------------------------------------------
 
-# TODO: just hardcode 11, 499 barcodes?
 barcodes_to_skip <-
   barcode_to_gene_map.txt %>%
   group_by(barcode_id) %>%
@@ -75,12 +74,9 @@ dstat <-
              stringsAsFactors = F)
 
 run_alg <- function(bid, FI, plate_name = NULL) {
-  # fc <- new("flexclustControl", iter.max = 10, verbose = 1,
-  #           initcent = "kmeanspp")
-  # cc <- cclust(FI, 2, dist = "euclidean", method = "kmeans",
-  #              control = fc)
 
-  FI <- FI[FI > 10] # filter low threshold
+  # TODO: filter upper threshold (?)
+  FI <- FI[FI > 10] # filter lower threshold
 
   distEuclidean <- function(x, centers)
   {
@@ -200,7 +196,7 @@ single_plate_processing <- function(filename) {
     d %>%
     group_by(barcode_id) %>%
     filter(!barcode_id %in% barcodes_to_skip) %>%
-    arrange(barcode_id) %>%
+    arrange(barcode_id) %>% # TODO: not arrange but return single column with names
     split(.$barcode_id)
 
   col <-
@@ -222,6 +218,61 @@ mat <- matrix(cols %>% unlist(),
               ncol = length(DATA.plates))
 rownames(mat) <- names(cols[[1]])
 colnames(mat) <- DATA.plates
+
+# voting ------------------------------------------------------------------
+
+genes_by_bids <- function(bids, d = barcode_to_gene_map.txt) {
+  result <-
+    d[d$barcode_id %in% as.integer(bids), ] %>%
+    arrange(desc(high_prop)) %>%
+    unlist()
+  result[3:4] %>% as.character()
+}
+
+for(bid in barcodes) {
+  genes <- genes_by_bids(bid, barcode_to_gene_map.txt)
+  gene_hi <- genes[1]
+  gene_lo <- genes[2]
+  ms.hi <- mat[gene_hi,]
+  ms.lo <- mat[gene_lo,]
+  ms.hi <- ms.hi[ms.hi > 0]
+  ms.lo <- ms.lo[ms.lo > 0]
+  mean.hi <- median(ms.hi)
+  mean.lo <- median(ms.lo)
+  sd.hi <- sd(ms.hi)
+  sd.lo <- sd(ms.lo)
+  for(well in names(ms.hi)) {
+    hi <- ms.hi[well]
+    lo <- ms.lo[well]
+    if(mean.lo - sd.lo <= hi && hi <= mean.lo + sd.lo &&
+       mean.hi - sd.hi <= lo && lo <= mean.hi + sd.hi) {
+      mat[gene_hi,well] <- lo
+      mat[gene_lo,well] <- hi
+    }
+  }
+}
+
+
+# mat <- apply(t(mat), c(2, 1), function(x) if(x < 0) { -x } else x)
+
+# for(bid in barcodes) {
+#   genes <- genes_by_bids(bid, barcode_to_gene_map.txt)
+#   gene_hi <- genes[1]
+#   gene_lo <- genes[2]
+#   ms.hi <- mat[gene_hi,]
+#   ms.lo <- mat[gene_lo,]
+#   ms.hi <- ms.hi[ms.hi > 0]
+#   ms.lo <- ms.lo[ms.lo > 0]
+#   mean.hi <- mean(ms.hi)
+#   mean.lo <- mean(ms.lo)
+#   for(well in names(ms.hi)) {
+#     hi <- ms.hi[well]
+#     if(hi < 0) {
+#       mat[gene_hi,well] <- mean.hi
+#       mat[gene_lo,well] <- mean.lo
+#     }
+#   }
+# }
 
 # save DATA ---------------------------------------------------------------
 
